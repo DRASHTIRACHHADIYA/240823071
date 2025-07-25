74ef6582e43cc278ab0508bbb4347aa2b4e1c149
DRASHTIRACHHADIYA <simapatel685@gmail.com>
2025-07-25 10:27:37 +0530

    node

---
+++ e2_maximum.js
@@ -0,0 +1,6 @@
+let a=5,b=9;
+if(a>b){
+    console.log("a is bigger")
+}else{
+    console.log("b is bigger")
+}
---
+++ e3_leap_year.js
@@ -0,0 +1,6 @@
+const year=2020;
+if(year%4==0){
+    console.log("this is leap year")
+}else{
+    console.log("this is not a leap year")
+}
